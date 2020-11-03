fbd6a4e2ba288d7b049fe266b3f87c0aa409f88f
Sean Owen <sean_owen@live.co.uk>
2020-11-03 21:05:46 +0000

    - start to implement tournament logic (unfinished)

--- src/TrackerBLP/TournamentLogic.cs
+++ src/TrackerBLP/TournamentLogic.cs
@@ -9,7 +9,7 @@ namespace TrackerBLP
     {
         public static void CreateRounds(Tournament tournament)
         {
-            // TODO - functionality - implement create rounds & subsequently required methods
+            // Dummy data
             tournament.Rounds = new List<List<Matchup>>()
             {
                 new List<Matchup>()
@@ -21,7 +21,7 @@ namespace TrackerBLP
                     new Matchup()
                     {
                         Id = 2
-                    }                    
+                    }
                 },
                 new List<Matchup>()
                 {
@@ -36,6 +36,46 @@ namespace TrackerBLP
                 }
             };
 
+            // TODO - functionality - implement create rounds & subsequently required methods
+
+            // 1 - calculate number of rounds based on teams
+            int numberOfRounds = CalculateNumRounds(tournament.EnteredTeams.Count);
+
+            // add round model to make life easier? -----------------
+
+            // 2 - create matchup list for each round
+            // 3 - add matchup entries for first round only (randomly select pairings and 'byes')
+            tournament.Rounds = new List<List<Matchup>>();
+            for (int i = 0; i < numberOfRounds; i++)
+            {
+                var roundMatchups = new List<Matchup>();
+                if (i == 0)
+                {
+                    // randomly select matchups for first round
+                    roundMatchups = CalculateFirstRoundMatchups();
+                }
+                tournament.Rounds.Add(roundMatchups);
+            }
+        }
+
+        private static List<Matchup> CalculateFirstRoundMatchups()
+        {
+            // TODO - functionality - implement first round creator method (this one!)
+            throw new NotImplementedException();
+        }
+
+        private static int CalculateNumRounds(int numberOfEnteredTeams)
+        {
+            var roundsIfEvenNumTeams = numberOfEnteredTeams / 2;
+            var leftOverTeams = numberOfEnteredTeams % 2;
+
+            int rounds = roundsIfEvenNumTeams;
+            if (leftOverTeams != 0)
+            {
+                rounds += 1;
+            }
+
+            return rounds;
         }
     }
 }
